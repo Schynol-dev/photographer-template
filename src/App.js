a05135358d772af7964eb974b955ec1286691074
Schynol-dev <kamil.schynol0dev@gmail.com>
2021-03-22 21:39:38 +0100

    Adding Navigation, Navigation's styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import "./assets/css/main.css";
+
+import Navigation from "./components/Navigation/Navigation";
 
 function App() {
   return (
-    <div></div>
+    <>
+      <Navigation />
+    </>
   );
 }
 
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -3,7 +3,20 @@ import React from "react";
 function Navigation() {
 
     return (
-        <div id="nav"></div>
+        <div id="nav">
+            <div className="logo">
+                <h3>Photographer</h3>
+            </div>
+            <div className="list">
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#home">What I Do</a></li>
+                    <li><a href="#home">About Me</a></li>
+                    <li><a href="#home">My Work</a></li>
+                    <li><a href="#home">Contact</a></li>
+                </ul>
+            </div>
+        </div>
     );
 }
 
