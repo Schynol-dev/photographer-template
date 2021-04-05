ab60ab71ff457e46783a21d3099b68d3ef3d60e0
Schynol-dev <kamil.schynol0dev@gmail.com>
2021-04-05 20:16:22 +0200

    Adding smooth scroll, react-scroll

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 import "./assets/css/main.css";
 
 import Navigation from "./components/Navigation/Navigation";
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,6 +1,26 @@
 import React from "react";
+import { animateScroll as scroll } from "react-scroll";
 
 function Navigation() {
+
+    // Section Y positions
+    const sectionPositionY = (section) => {
+        switch(section) {
+            case "home":
+               return document.querySelector("#home").getBoundingClientRect().top + window.screenY - document.body.getBoundingClientRect().top;
+            case "do":
+                return  document.querySelector("#do").getBoundingClientRect().top + window.screenY - document.body.getBoundingClientRect().top;
+            case "about":
+                return  document.querySelector("#about").getBoundingClientRect().top + window.screenY - document.body.getBoundingClientRect().top;
+            case "work":
+                return  document.querySelector("#work").getBoundingClientRect().top + window.screenY - document.body.getBoundingClientRect().top;
+            case "contact":
+                return document.querySelector("#contact").getBoundingClientRect().top + window.screenY - document.body.getBoundingClientRect().top;
+            default:
+                return window.screenY; 
+        }
+        
+    }
 
     return (
         <div id="nav">
@@ -9,11 +29,11 @@ function Navigation() {
             </div>
             <div className="list">
                 <ul>
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#do">What I Do</a></li>
-                    <li><a href="#about">About Me</a></li>
-                    <li><a href="#work">My Work</a></li>
-                    <li><a href="#contact">Contact</a></li>
+                    <li><a href="#home" onClick={() => scroll.scrollTo(sectionPositionY("home"))}>Home</a></li>
+                    <li><a href="#do" onClick={() => scroll.scrollTo(sectionPositionY("do"))}>What I Do</a></li>
+                    <li><a href="#about" onClick={() => scroll.scrollTo(sectionPositionY("about"))}>About Me</a></li>
+                    <li><a href="#work" onClick={() => scroll.scrollTo(sectionPositionY("work"))}>My Work</a></li>
+                    <li><a href="#contact" onClick={() => scroll.scrollTo(sectionPositionY("contact"))}>Contact</a></li>
                 </ul>
             </div>
         </div>
