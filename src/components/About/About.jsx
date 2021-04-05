9ea52646dcd930b30d9f65873bdb73878a90e854
Schynol-dev <kamil.schynol0dev@gmail.com>
2021-04-05 19:13:02 +0200

    Adding animation

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useRef } from "react";
+import {useIntersection}  from "react-use"
+
+import { fadeIn, fadeOut } from "../../utils/gsap/fade";
 
 function About() {
 
+    const intersectionRef = useRef(null);
+
+    const intersection = useIntersection(intersectionRef, {
+        root: null,
+        rootMargin: '0px',
+        threshold: .5
+    });
+
+    intersection && intersection.intersectionRatio < .5 ? fadeOut(".fade-about") : fadeIn(".fade-about");
+
     return (
-        <section id="about">
+        <section ref={intersectionRef} id="about">
             <div className="container wrapper">
-                <div className="content">
-                    <h2>Get To Know Me</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eros nunc, fringilla tincidunt erat vitae, pulvinar suscipit felis. 
+                <div className="content fade-about">
+                    <h2 className="fade-about">Get To Know Me</h2>
+                    <p className="fade-about">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eros nunc, fringilla tincidunt erat vitae, pulvinar suscipit felis. 
                         Fusce ipsum ex, tempus ac velit id, venenatis consequat ligula. Sed bibendum auctor magna, at facilisis massa eleifend eget.
                         Mauris vehicula ante id auctor rhoncus. Mauris in nunc auctor diam sodales euismod. Sed blandit pretium sem, id placerat sem consectetur in. 
                         Mauris vulputate velit mauris, eget malesuada diam facilisis nec. Fusce tincidunt libero vel magna convallis convallis. 
