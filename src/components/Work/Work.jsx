3a84045813cb22c2b8501d5c408f84e60cc6f164
Schynol-dev <kamil.schynol0dev@gmail.com>
2021-04-06 11:48:52 +0200

    Work Section, adding fade

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { useRef } from "react";
+import { useIntersection }  from "react-use"
+
+import { fadeIn, fadeOut } from "../../utils/gsap/fade";
 
 function Work() {
 
+    const intersectionRef = useRef(null);
+
+    const intersection = useIntersection(intersectionRef, {
+        root: null,
+        rootMargin: '0px',
+        threshold: .3
+    });
+
+    intersection && intersection.intersectionRatio < .3 ? fadeOut(".fade-work") : fadeIn(".fade-work");
+
     return (
-        <section id="work">
+        <section ref={intersectionRef} id="work">
             <div className="container wrapper">
-                <h2>See My Work</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eros nunc, fringilla tincidunt erat vitae, pulvinar suscipit felis. 
+                <h2 className="fade-work">See My Work</h2>
+                <p className="fade-work">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eros nunc, fringilla tincidunt erat vitae, pulvinar suscipit felis. 
                     Fusce ipsum ex, tempus ac velit id, venenatis consequat ligula. Sed bibendum auctor magna, at facilisis massa eleifend eget.</p>
                 <div className="photos">
-                    <div className="item one"></div>
-                    <div className="item two"></div>
-                    <div className="item three"></div>
-                    <div className="item four"></div>
-                    <div className="item five"></div>
-                    <div className="item six"></div>
+                    <div className="item one fade-work"></div>
+                    <div className="item two fade-work"></div>
+                    <div className="item three fade-work"></div>
+                    <div className="item four fade-work"></div>
+                    <div className="item five fade-work"></div>
+                    <div className="item six fade-work"></div>
                 </div>
             </div>
         </section>
